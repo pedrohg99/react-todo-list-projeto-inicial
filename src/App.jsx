31d08c01566025d808f1c0db2d1cd8e69237289f
Pedro Henrique <pedrohg99@yahoo.com>
2026-05-03 18:50:28 -0300

    Aplica funcionalidade de EmptyState

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { IconPlus, IconSchool } from "./components/icons";
 import { ToDoForm } from "./components/ToDoForm";
 import { TodoGroup } from "./components/TodoGroup";
 import TodoContext from "./components/ToDoProvider/todoContext";
+import { EmptyState } from "./components/EmptyState";
 
 function App() {
   const { todos, addTodo, showDialog, openFormTodoDialog, closeFormTodoDialog, selectedTodo, editTodo } = use(TodoContext);
@@ -36,6 +37,7 @@ function App() {
             heading="Para estudar"
             items={todos.filter((t) => !t.completed)}
           />
+          {todos.length == 0 && <EmptyState />} 
           <TodoGroup
             heading="Concluído"
             items={todos.filter((t) => t.completed)}
